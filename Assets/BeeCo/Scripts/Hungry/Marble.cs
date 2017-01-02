b449f0d674761c0d12fdab700e238e39a3d09c44
EntranceJew <EntranceJew@gmail.com>
2017-01-02 16:55:29 -0500

    Prefabbed God, did something, Marble has a script now.

---
+++ Assets/BeeCo/Scripts/Hungry/Marble.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class Marble : MonoBehaviour {
+    public float value = 1.00f;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
