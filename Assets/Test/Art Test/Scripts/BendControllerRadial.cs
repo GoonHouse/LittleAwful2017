07c137b4293aec5b170ed81050fd0c185632b383
TomRintjema <tomrintjema@hotmail.com>
2017-01-03 23:03:15 -0500

    Horizon Curving Shader Stuff

---
+++ Assets/Test/Art Test/Scripts/BendControllerRadial.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class BendControllerRadial : MonoBehaviour {
+
+	[SerializeField] Transform m_curveOrigin;
+	[SerializeField] Transform m_referenceDirection;
+	[SerializeField] float m_curvature = 0f;
+
+	[Range(0.5f, 2f)]
+	[SerializeField] float m_xScale = 1f;
+	[Range(0.5f, 2f)]
+	[SerializeField] float m_zScale = 1f;
+
+	[SerializeField] float m_flatMargin = 0f;
+
+	[Range(0f, 10f)]
+	[SerializeField] float m_horizonWaveFrequency = 0f;
+
+	private int m_curveOriginId;
+	private int m_referenceDirectionId;
+	private int m_curvatureId;
+	private int m_scaleId;
+	private int m_flatMarginId;
+	private int m_horizonWaveFrequencyId;
+
+	private Vector3 m_scale = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+		m_curveOriginId = Shader.PropertyToID ("_CurveOrigin");
+		m_referenceDirectionId = Shader.PropertyToID ("_ReferenceDirection");
+		m_curvatureId = Shader.PropertyToID ("_Curvature");
+		m_scaleId = Shader.PropertyToID ("_Scale");
+		m_flatMarginId = Shader.PropertyToID ("_FlatMargin");
+		m_horizonWaveFrequencyId = Shader.PropertyToID ("_HorizonWaveFrequency");
+
+		//if (m_curveOrigin == null)
+			//SetCurveOrigin (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		m_scale.x = m_xScale;
+		m_scale.z = m_zScale;
+
+		Shader.SetGlobalVector (m_curveOriginId, m_curveOrigin.position);
+		Shader.SetGlobalVector (m_referenceDirectionId, m_referenceDirection.forward);
+		Shader.SetGlobalFloat (m_curvatureId, m_curvature * 0.00001f);
+		Shader.SetGlobalVector (m_scaleId, m_scale);
+		Shader.SetGlobalFloat (m_flatMarginId, m_flatMargin);
+		Shader.SetGlobalFloat (m_horizonWaveFrequencyId, m_horizonWaveFrequency);
+	}
+	/*
+	private void SetCurveOrigin(bool freeCamera)
+	{
+		//m_curveOrigin = GameController.CurveOrigin;
+	}
+
+	private void OnEnable()
+	{
+		//EventManager.StartListening (EventNames.ToggleFreeCamera, SetCurveOrigin);
+	}
+
+	private void OnDisable()
+	{
+		//EventManager.StopListening (EventNames.ToggleFreeCamera, SetCurveOrigin);
+
+		Shader.SetGlobalVector (m_curveOriginId, Vector3.zero);
+		Shader.SetGlobalFloat (m_curvatureId, 0);
+	}
+	*/
+}
---
+++ Assets/Test/Art Test/Shaders/Bend.shader
@@ -0,0 +1,78 @@
+﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+// Upgrade NOTE: replaced '_World2Object' with 'unity_WorldToObject'
+
+Shader "Custom/Bend" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_Glossiness ("Smoothness", Range(0,1)) = 0.5
+		_Metallic ("Metallic", Range(0,1)) = 0.0
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma surface surf Standard fullforwardshadows vertex:vert
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		uniform half3 _CurveOrigin;
+		uniform fixed3 _ReferenceDirection;
+		uniform half _Curvature;
+		uniform fixed3 _Scale;
+		uniform half _FlatMargin;
+		uniform half _HorizonWaveFrequency;
+
+		sampler2D _MainTex;
+
+		struct Input {
+			float2 uv_MainTex;
+		};
+
+		half4 Bend(half4 v)
+		{
+			half4 wpos = mul (unity_ObjectToWorld, v);
+
+			half2 xzDist = (wpos.xz - _CurveOrigin.xz) / _Scale.xz;
+			half dist = length(xzDist);
+
+			half2 direction = lerp(_ReferenceDirection.xz, xzDist, min(dist, 1));
+
+			half theta = acos(clamp(dot(normalize(direction), _ReferenceDirection.xz), -1, 1));
+
+			dist = max(0, dist - _FlatMargin);
+
+			wpos.y -= dist * dist * _Curvature * cos(theta * _HorizonWaveFrequency);
+
+			wpos = mul (unity_WorldToObject, wpos);
+
+			return wpos;
+		}
+
+		void vert (inout appdata_full v)
+		{
+			half4 vpos = Bend(v.vertex);
+
+			v.vertex = vpos;
+		}
+
+		half _Glossiness;
+		half _Metallic;
+		fixed4 _Color;
+
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+			// Albedo comes from a texture tinted by color
+			fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+			o.Albedo = c.rgb;
+			// Metallic and smoothness come from slider variables
+			o.Metallic = _Metallic;
+			o.Smoothness = _Glossiness;
+			o.Alpha = c.a;
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
