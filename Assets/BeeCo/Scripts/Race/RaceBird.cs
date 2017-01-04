1b3191aae047aab08123a8aea42cfa21824bb055
EntranceJew <EntranceJew@gmail.com>
2017-01-03 19:46:39 -0500

    reduced bird escape

--- Assets/BeeCo/Scripts/Race/RaceBird.cs
+++ Assets/BeeCo/Scripts/Race/RaceBird.cs
@@ -9,37 +9,74 @@ public class RaceBird : MonoBehaviour {
 
     public Vector2 moveDist = new Vector2(14.5f, 13.7f);
 
-	// Use this for initialization
-	void Start () {
-	    
-	}
+    public Vector2 minExtents = new Vector2(1, 0);
+    public Vector2 maxExtents = new Vector2(4, 7);
+
+    // Use this for initialization
+    void Start () {
+        SetPosition(lane, position);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if ( brain.BrainLeft() ) {
-            CanMove(lane - 1, position);
+            MoveIfPossible(lane - 1, position);
         } else if ( brain.BrainRight() ) {
-            CanMove(lane + 1, position);
+            MoveIfPossible(lane + 1, position);
         }
 
 
         if ( brain.BrainGo() ) {
-            CanMove(lane, position + 1);
+            MoveIfPossible(lane, position + 1);
         } else if ( brain.BrainStop() ) {
-            CanMove(lane, position - 1);
+            MoveIfPossible(lane, position - 1);
         }
     }
 
-    // burp
+    void SetPosition(int newLane, int newPosition) {
+        var rg = God.main.GetComponent<RaceGod>();
+        rg.grid.Add(newLane.ToString() + "_" + newPosition.ToString(), this.gameObject);
+        this.lane = newLane;
+        this.position = newPosition;
+    }
+
+    void ClearPosition(int newLane, int newPosition) {
+        var rg = God.main.GetComponent<RaceGod>();
+        rg.grid.Remove(newLane.ToString() + "_" + newPosition.ToString());
+    }
+
     bool CanMove(int moveLane, int movePosition) {
-        var t = GetComponent<Transform>();
-        var pos = t.position;
-        var mvec = new Vector2(lane - moveLane,  movePosition - position);
-        lane = moveLane;
-        position = movePosition;
-        pos.x += mvec.y * moveDist.x;
-        pos.z += mvec.x * moveDist.y;
-        t.position = pos;
-        return true;
+        var rg = God.main.GetComponent<RaceGod>();
+        bool canMove = true;
+        
+        // is it occupied?
+        GameObject doesExist;
+        rg.grid.TryGetValue(moveLane.ToString() + "_" + movePosition.ToString(), out doesExist);
+        canMove = (canMove && (doesExist == null));
+
+        // is it within boundaries?
+        canMove = (canMove && ( moveLane >= minExtents.x && moveLane <= maxExtents.x ));
+        canMove = (canMove && ( movePosition >= minExtents.y && movePosition <= maxExtents.y));
+
+        return canMove;
+    }
+
+    bool MoveIfPossible(int moveLane, int movePosition) {
+        if( CanMove(moveLane, movePosition)) {
+            var t = GetComponent<Transform>();
+            var pos = t.position;
+            var mvec = new Vector2(lane - moveLane, movePosition - position);
+            ClearPosition(lane, position);
+            lane = moveLane;
+            position = movePosition;
+            pos.x += mvec.y * moveDist.x;
+            pos.z += mvec.x * moveDist.y;
+            t.position = pos;
+            SetPosition(lane, position);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- Assets/BeeCo/Scripts/Race/RaceGod.cs
+++ Assets/BeeCo/Scripts/Race/RaceGod.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class RaceGod : MonoBehaviour {
-    Dictionary<string, GameObject> grid = new Dictionary<string, GameObject>();
+    public Dictionary<string, GameObject> grid = new Dictionary<string, GameObject>();
 
 	// Use this for initialization
 	void Start () {
