7d7fb3a490858b897a88c72b4525e4c94ff7656e
Seppi <josefnpat@gmail.com>
2017-01-09 18:19:19 -0600

    Adding a fun menu
    * Adding a Handle Ray Click script of some sort
    * Adding new font (will try and use this for GUI etc)
    * Throwing the options onto the floor

---
+++ Assets/BeeCo/Scripts/HandleRayClick.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class HandleRayClick : MonoBehaviour {
+
+    private Material default_mat = null;
+    public Material hover_mat = null;
+    public Material click_mat = null;
+
+    public string target_state = "";
+    public bool quit = false;
+    public GameObject toggle_panel = null;
+
+    // Use this for initialization
+    void Start () {
+        default_mat = GetComponent<Renderer>().material;//        gameObject.GetComponent<Material>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnMouseDown() {
+        GetComponent<Renderer>().material = click_mat;
+    }
+
+    public void OnMouseUp() {
+        if(target_state != "") {
+            SceneManager.LoadScene(target_state);
+        }
+        if(quit) {
+            Application.Quit();
+        }
+        if(toggle_panel) {
+            toggle_panel.SetActive(!toggle_panel.activeSelf);
+        }
+    }
+
+    public void OnMouseEnter() {
+        GetComponent<Renderer>().material = hover_mat;
+    }
+
+    public void OnMouseExit() {
+        GetComponent<Renderer>().material = default_mat;
+    }
+
+}
