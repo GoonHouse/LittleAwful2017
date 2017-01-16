8eb30435300b26973bf3e862941e67fda92c2b76
Seppi <josefnpat@gmail.com>
2017-01-15 21:09:45 -0600

    Updating brand and adding drug scene on quit

---
+++ Assets/BeeCo/Scripts/QuitAfterTime.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitAfterTime : MonoBehaviour {
+
+    private float dt = 0;
+    public float t = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.dt += Time.deltaTime;
+        if(dt > t) {
+            Application.Quit();
+            Debug.Break();
+            Debug.Log("Bye!");
+        }
+    }
+}
